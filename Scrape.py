cad45503567e061fd8a855662733f987aa9cfa43
itismejy <44042515+itismejy@users.noreply.github.com>
2019-03-02 11:59:31 -0500

    Add files via upload
    Pulls Nasdaq data for 5 seconds, saves dataframe into csv

--- Scrape.py
+++ Scrape.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import time
+import csv
+import pandas as pd
+import numpy as np
+
 s=input("What ticker? ")
 website='https://finance.yahoo.com/quote/{}/'.format(s)
 page=requests.get(website)
@@ -11,7 +15,7 @@ col1 = soup.find_all('span', {'class': 'Trsdu(0.3s)'})
 cleanData=[]
 #cleanData=np.asarray(cleanData)
 a=0
-while a < 10:
+while a < 5:
     row=[]
     for x in range(len(col1)):
         row.append(col1[x].decode_contents(formatter="html"))
@@ -19,11 +23,13 @@ while a < 10:
     cleanData.append(row)
     #row = np.asarray(row)
     #np.stack((cleanData,row))
-    time.sleep(30)
+    time.sleep(1)
     print(cleanData)
-#price = soup.find('span', data-reactid_='35')
-#col2=soup.find_all('td', class_=class2)
-#print(col1.decode_contents(formatter="html"))
+
+numpyCD=np.array(cleanData)
+df=pd.DataFrame(numpyCD,columns=['Trading Price', 'Daily Change', 'Previous Close', 'Open', 'Bid', 'Ask', 'Volume', 'Avg. Volume', 'Market Cap', 'Beta (3Y Monthly)', 'PE Ratio (TTM)', 'EPS (TTM)', 'Ex-Dividend Date', '1y Target Est'])
+df.to_csv('data.csv')
+print (df)
 
 #Forward Dividend & Yield
 #Write a program to pull data from Apple's historical data
