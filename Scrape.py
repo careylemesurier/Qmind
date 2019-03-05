d399f55922d3a05935137c32ee9210fbcb4dfa62
itismejy <44042515+itismejy@users.noreply.github.com>
2019-03-05 18:14:00 -0500

    Add files via upload
    For after hour trading, quick fix. Code will not run now during trading hours. Permanent fix to come

--- Scrape.py
+++ Scrape.py
@@ -4,6 +4,7 @@ import time
 import csv
 import pandas as pd
 import numpy as np
+import datetime
 
 s=input("What ticker? ")
 website='https://finance.yahoo.com/quote/{}/'.format(s)
@@ -12,13 +13,19 @@ soup = bs(page.content, 'html.parser')
 class1='W(100%)'
 class2='Ta(end) Fw(b) Lh(14px)'
 col1 = soup.find_all('span', {'class': 'Trsdu(0.3s)'})
+#while market is closed
 cleanData=[]
 #cleanData=np.asarray(cleanData)
 a=0
 while a < 5:
     row=[]
+    currentDT=datetime.datetime.now()
+    print(currentDT)
+    row.append(currentDT.strftime("%m/%d/%Y"))
+    row.append(currentDT.strftime("%H:%M:%S"))
     for x in range(len(col1)):
-        row.append(col1[x].decode_contents(formatter="html"))
+        if (x!=2):
+            row.append(col1[x].decode_contents(formatter="html"))
     a+=1
     cleanData.append(row)
     #row = np.asarray(row)
@@ -27,7 +34,9 @@ while a < 5:
     print(cleanData)
 
 numpyCD=np.array(cleanData)
-df=pd.DataFrame(numpyCD,columns=['Trading Price', 'Daily Change', 'Previous Close', 'Open', 'Bid', 'Ask', 'Volume', 'Avg. Volume', 'Market Cap', 'Beta (3Y Monthly)', 'PE Ratio (TTM)', 'EPS (TTM)', 'Ex-Dividend Date', '1y Target Est'])
+print(numpyCD.shape)
+print(numpyCD)
+df=pd.DataFrame(numpyCD,columns=['Date', 'Time', 'Trading Price', 'Daily Change', 'Previous Close', 'Open', 'Bid', 'Ask', 'Volume', 'Avg. Volume', 'Market Cap', 'Beta (3Y Monthly)', 'PE Ratio (TTM)', 'EPS (TTM)', 'Ex-Dividend Date', '1y Target Est'])
 df.to_csv('data.csv')
 print (df)
 
